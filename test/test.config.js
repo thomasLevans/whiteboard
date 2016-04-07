4e6d970d74aca343ccf1cf2124cd63b3f26cc0ef
Tom Evans <thomasLevans@users.noreply.github.com>
2016-04-07 08:14:56 -0400

    Issue #7 Edit Operations - Basic

--- test/test.config.js
+++ test/test.config.js
@@ -1,4 +1,28 @@
 import chai from 'chai';
+import jsdom from 'jsdom';
 import chaiDeepMatch from 'chai-deep-match';
 
+/*
+* Test config file:
+*
+* creates jsdom versions of document and window objects
+* takes all props of the json window obj and hoists them on to
+* the Node.js global object - this is so props provided by window
+* can be used without the window
+*
+*/
+
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const win = doc.defaultView;
+
+global.document = doc;
+global.window = win;
+
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
+});
+
 chai.use(chaiDeepMatch);
+chai.config.truncateThreshold = 0;
